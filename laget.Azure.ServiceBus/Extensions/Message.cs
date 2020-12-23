4427ec1b7b58994a04c50d94d269a70889579bd3
Erik Hennerfors <erik.hennerfors@obscured.se>
2020-12-23 08:34:04 +0100

    Added extension to deserialize Microsoft.Azure.ServiceBus.Message

---
+++ laget.Azure.ServiceBus/Extensions/Message.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace laget.Azure.ServiceBus.Extensions
+{
+    public static class MessageExtension
+    {
+        public static TEntity Deserialize<TEntity>(this Microsoft.Azure.ServiceBus.Message message) where TEntity : Message
+        {
+            var entity = JsonConvert.DeserializeObject<TEntity>(Encoding.UTF8.GetString(message.Body),
+                new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Auto
+                });
+
+            entity.Id = message.MessageId;
+            entity.Source = message;
+
+            return entity;
+        }
+    }
+}
