6398d9c937be9484a8ba34b3c052279d71c3c621
Emil Söderling <emil.soderling@b3.se>
2021-06-10 15:45:00 +0200

    Added missing test for ToServiceMessage extension method
    These changes will add some reflection setup of Azure Servicebus
    Message, which more or less give us a "true" message in the sense for
    testing.

--- laget.Azure.ServiceBus.Tests/Extensions/MessageTests.cs
+++ laget.Azure.ServiceBus.Tests/Extensions/MessageTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Reflection;
+using System.Text;
 using laget.Azure.ServiceBus.Extensions;
 using Xunit;
 
@@ -6,14 +8,51 @@ namespace laget.Azure.ServiceBus.Tests.Extensions
 {
     public class MessageTests
     {
+        private Microsoft.Azure.ServiceBus.Message _servicebusMessage;
+
+        public MessageTests()
+        {
+            // One-time setup
+            // Only use this setup for testing, do not attempt to set systemproperties in production
+            _servicebusMessage = new Microsoft.Azure.ServiceBus.Message { MessageId = "SomeCoolId1" };
+
+            var systemProperties = new Microsoft.Azure.ServiceBus.Message.SystemPropertiesCollection();
+            var bindings = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty;
+
+            var sequenceNumber = 1;
+            var enqueueTime = DateTime.Now.AddSeconds(5);
+            systemProperties.GetType().InvokeMember("EnqueuedTimeUtc", bindings, Type.DefaultBinder, systemProperties, new object[] { enqueueTime } );
+            systemProperties.GetType().InvokeMember("SequenceNumber", bindings, Type.DefaultBinder, systemProperties, new object[] { sequenceNumber });
+
+            // Set mocked-up systemprorperties for current message
+            bindings = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty;
+            _servicebusMessage.GetType().InvokeMember("SystemProperties", bindings, Type.DefaultBinder, _servicebusMessage, new object[] { systemProperties });
+        }
+
         [Fact]
         public void ShouldDeserializeMessage()
         {
-            var body = Encoding.UTF8.GetBytes("{ \"name\": \"Jane Doe\" }");
-            var message = new Microsoft.Azure.ServiceBus.Message { Body = body };
+            _servicebusMessage.Body = Encoding.UTF8.GetBytes("{ \"Name\": \"Jane Doe\" }");
+            var model = _servicebusMessage.Deserialize<Models.User>();
 
-            var model = message.Deserialize<Models.User>();
+            Assert.Equal("Jane Doe", model.Name);
+        }
 
+        [Fact]
+        public void ShouldConvertMessage()
+        {
+            var userMessage = new Models.User
+            {
+                Category = "UserCategory",
+                CreatedAt = DateTime.Now,
+                Id = "UserId1",
+                Name = "Jane Doe",
+                Type = "UserType"
+            };
+
+            var servicebusMessage = userMessage.ToServicebusMessage();
+            var model = servicebusMessage.Deserialize<Models.User>();
+        
             Assert.Equal("Jane Doe", model.Name);
         }
     }
