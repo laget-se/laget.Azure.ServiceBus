1e260022e9609468d13a36ec22a2f1ac701b2353
Erik Hennerfors <erik.hennerfors@laget.se>
2023-10-05 09:11:09 +0200

    Update(s)

--- laget.Azure.ServiceBus.Tests/Extensions/MessageTests.cs
+++ laget.Azure.ServiceBus.Tests/Extensions/MessageTests.cs
@@ -7,25 +7,6 @@ namespace laget.Azure.ServiceBus.Tests.Extensions
 {
     public class MessageTests
     {
-        //public MessageTests()
-        //{
-        //    // One-time setup
-        //    // Only use this setup for testing, do not attempt to set system properties in production
-        //    _serviceBusMessage = new ServiceBusMessage { MessageId = "SomeCoolId1" };
-
-        //    var systemProperties = new Microsoft.Azure.ServiceBus.Message.SystemPropertiesCollection();
-        //    var bindings = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty;
-
-        //    var sequenceNumber = 1;
-        //    var enqueueTime = DateTime.Now.AddSeconds(5);
-        //    systemProperties.GetType().InvokeMember("EnqueuedTimeUtc", bindings, Type.DefaultBinder, systemProperties, new object[] { enqueueTime });
-        //    systemProperties.GetType().InvokeMember("SequenceNumber", bindings, Type.DefaultBinder, systemProperties, new object[] { sequenceNumber });
-
-        //    // Set mocked-up system properties for current message
-        //    bindings = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty;
-        //    _serviceBusMessage.GetType().InvokeMember("SystemProperties", bindings, Type.DefaultBinder, _serviceBusMessage, new object[] { systemProperties });
-        //}
-
         [Fact]
         public void ShouldDeserializeMessage()
         {
